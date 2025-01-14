bc6a3a76a54b26514ac4a113eecb16f69bfa8b9b
lee juyoung <juyung0903@naver.com>
2025-01-14 12:43:55 +0900

    feat: 더미데이터로 페이지 파라미터에 따른 데이터 가져오기 & 프렌즈타입에 따른 이미지 다르게 보여주기

--- src/app/(afterLogin)/friends/[id]/page.tsx
+++ src/app/(afterLogin)/friends/[id]/page.tsx
@@ -1,21 +1,24 @@
 import Button from '@/app/_component/ui/Button';
 import Image from 'next/image';
+import { findFriendById } from './_lib/dummyData';
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = await params;
+  const data = findFriendById(Number(id));
+  console.log(data?.friendType)
   // 서버 컴포넌트는 다름
   return (
     <div className="mt-[147px] flex w-full flex-col">
       <div className="relative flex w-full flex-col items-start bg-areaBg p-5">
         <Image
-          src="/image/friend_detail_type.png"
+          src={`/image/friend_type_${data?.friendType}.png`}
           alt="상세정보 프렌즈 타입"
           width={168}
           height={213}
           className="absolute top-0 right-5 transform -translate-y-1/2"
         />
-        <div className="text-sm font-medium leading-[19.6px] text-textLess">공연 한줄소개</div>
-        <div className="text-[28px] font-semibold leading-[39.2px]">뭘 봐!</div>
-        <div className="mt-5 flex items-start gap-4 text-sm leading-[19.6px]">
+        <div className="text-sm font-medium leading-[19.6px] text-textLess">{data?.introduction}</div>
+        <div className="text-[28px] font-semibold leading-[39.2px]">{data?.name}</div>
+        <div className="mt-6 flex items-start gap-4 text-sm leading-[19.6px]">
           <div className="flex flex-col gap-2 text-textLess">
             <div>성별</div>
             <div>음악 취향</div>
@@ -23,15 +26,15 @@ export default async function Page({ params }: { params: { id: string } }) {
             <div>MBTI</div>
           </div>
           <div className="flex flex-col gap-2">
-            <div>남</div>
-            <div>음악 취향</div>
-            <div>감상 타입</div>
-            <div>MBTI</div>
+            <div>{data?.gender}</div>
+            <div>{data?.taste}</div>
+            <div>{data?.applicationType}</div>
+            <div>{data?.mbti}</div>
           </div>
         </div>
         <div className="mt-12 w-full flex justify-between">
           <div className="text-[16px] leading-[22.4px]">
-            <span className="text-[40px] font-semibold leading-[56px] text-primary">72</span>
+            <span className="text-[40px] font-semibold leading-[56px] text-primary">{data?.volume}</span>
             <span className="text-[28px] font-semibold leading-[39.2px] text-primary"> </span>
             <span>Volume</span>
           </div>
