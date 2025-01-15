7bde2f8f69230362098b6920ec90f4fb014a674e
Kim HeeChan <h22chan.dev@gmail.com>
2025-01-16 02:35:27 +0900

    refactor: 프렌즈 상세 페이지 CSS 수정

--- src/app/(afterLogin)/friends/[id]/page.tsx
+++ src/app/(afterLogin)/friends/[id]/page.tsx
@@ -1,10 +1,12 @@
 import Button from '@/app/_component/ui/Button';
 import Image from 'next/image';
 import { findFriendById } from './_lib/dummyData';
+
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = await params;
   const data = findFriendById(Number(id));
-  console.log(data?.friendType)
+  console.log(data?.friendType);
+
   // 서버 컴포넌트는 다름
   return (
     <div className="mt-[147px] flex w-full flex-col">
@@ -14,11 +16,11 @@ export default async function Page({ params }: { params: { id: string } }) {
           alt="상세정보 프렌즈 타입"
           width={168}
           height={213}
-          className="absolute top-0 right-5 transform -translate-y-1/2"
+          className="absolute right-5 top-0 -translate-y-1/2 transform"
         />
-        <div className="text-sm font-medium leading-[19.6px] text-textLess">{data?.introduction}</div>
-        <div className="text-[28px] font-semibold leading-[39.2px]">{data?.name}</div>
-        <div className="mt-6 flex items-start gap-4 text-sm leading-[19.6px]">
+        <div className="text-sm text-textLess">{data?.introduction}</div>
+        <div className="text-2xl font-semibold">{data?.name}</div>
+        <div className="mt-5 flex items-start gap-5 text-sm">
           <div className="flex flex-col gap-2 text-textLess">
             <div>성별</div>
             <div>음악 취향</div>
@@ -32,13 +34,13 @@ export default async function Page({ params }: { params: { id: string } }) {
             <div>{data?.mbti}</div>
           </div>
         </div>
-        <div className="mt-12 w-full flex justify-between">
-          <div className="text-[16px] leading-[22.4px]">
-            <span className="text-[40px] font-semibold leading-[56px] text-primary">{data?.volume}</span>
-            <span className="text-[28px] font-semibold leading-[39.2px] text-primary"> </span>
-            <span>Volume</span>
+        <div className="mt-12 flex w-full items-end justify-between gap-4">
+          <div className="text-base">
+            <span className="text-[40px] font-semibold text-primary">{data?.volume}</span> Volume
           </div>
-          <div className='pt-2 pb-1 h-full flex-1 max-w-[300px]'><Button text='공연 친구 맺기' variant='secondary'/></div>
+          <div className="h-full max-w-[300px] flex-1">
+            <Button text="공연 친구 맺기" variant="secondary" />
+          </div>
         </div>
       </div>
     </div>
