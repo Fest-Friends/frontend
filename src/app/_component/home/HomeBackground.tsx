3f3f8c40a214ca11cd00dc2b45f517ccb78f2e7b
lee juyoung <juyung0903@naver.com>
2025-01-17 23:36:37 +0900

    refactor: title과 subtitle 그룹핑 & 마진이 아닌 gap으로 두 요소간 거리조절

--- src/app/_component/home/HomeBackground.tsx
+++ src/app/_component/home/HomeBackground.tsx
@@ -4,17 +4,19 @@ export function HomeBackground() {
 
 
   return (
-    <div className="w-full py-20 flex flex-col gap-2 items-center text-sm leading-[19.6px] bg-home bg-no-repeat bg-cover"
-    >
-        <div className="text-2xl leading-[33.6px]">
-          공연 <span className="text-primary font-semibold">같이 볼 프렌즈</span>가 필요한가요?
-        </div>
-        <div className="text-textLess">
-        공연을 위한 나만의 페스트프렌즈를 찾아보세요!
-        </div>
-        <div className="mt-6">
-        <Button  variant="default" width="111px" text="프렌즈 찾기"/>
-        </div>
+    <div className="flex w-full flex-col items-center gap-6 bg-home bg-cover bg-no-repeat px-5 py-20 text-sm leading-[19.6px]">
+    {/* 타이틀과 서브타이틀 한 번 그룹핑 */}
+    <div className="w-full flex-col flex gap-1 text-center">
+      <div className="text-2xl leading-[33.6px]">
+        공연 <span className="font-semibold text-primary">같이 볼 프렌즈</span>가 필요한가요?
+      </div>
+      <div className="text-textLess">공연을 위한 나만의 페스트프렌즈를 찾아보세요!</div>
     </div>
+  
+    {/* 버튼 DIV 상위에서 gap-6을 통해 제목과 버튼 두 콘텐츠 간격을 조정 */}
+    <div className="justify-center flex w-full">
+      <Button variant="default" width="111px" text="프렌즈 찾기" />
+    </div>
+  </div>
   )
 }
